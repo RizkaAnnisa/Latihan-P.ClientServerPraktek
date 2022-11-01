f2c04bb1404c0b4e3b335e87c01c07f18f072869
Rizka Annisa <113493757+RizkaAnnisa@users.noreply.github.com>
2022-11-01 16:34:06 -0700

    Add files via upload
    Project : Maven Project
    Spring Boot : 2.7.5
    Java : 17
    Dependencies :
    Spring Data JPA
    Lombok
    Spring Web
    H2 Database
    
    JDK : 17.0.4

---
+++ peminjaman-service/src/main/java/com/rizka/peminjaman/VO/ResponseTemplateVO.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rizka.peminjaman.VO;
+
+import com.rizka.peminjaman.entity.Peminjaman;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author DELL
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseTemplateVO {
+    private Peminjaman peminjaman;
+    private Anggota anggota;
+
+    public void setAnggota(Anggota anggota) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setPeminjaman(Peminjaman peminjaman) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
+
+
---
+++ peminjaman-service/src/main/java/com/rizka/peminjaman/service/PeminjamanService.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rizka.peminjaman.service;
+
+import com.rizka.peminjaman.VO.Anggota;
+import com.rizka.peminjaman.VO.ResponseTemplateVO;
+import com.rizka.peminjaman.entity.Peminjaman;
+import com.rizka.peminjaman.repository.PeminjamanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author DELL
+ */
+@Service
+public class PeminjamanService {
+    @Autowired
+    private PeminjamanRepository peminjamanRepository;
+    
+    @Autowired
+    private RestTemplate restTemplate;
+    
+    public Peminjaman savePeminjaman(Peminjaman peminjaman) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    public ResponseTemplateVO getPeminjaman(Long peminjamanId){
+        ResponseTemplateVO vo = new ResponseTemplateVO();
+        Peminjaman peminjaman = peminjamanRepository.findByPeminjamanId(peminjamanId);
+        
+        Anggota anggota = restTemplate.getForObject("http://localhost:9001/anggota/"+peminjaman.getAnggotaId(), Anggota.class);
+        vo.setPeminjaman(peminjaman);
+        vo.setAnggota(anggota);
+        return vo;
+                
+    }
+}
